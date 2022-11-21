71b9571fca8a9b02f32dcf2030e93f3fc70012d9
Moin Khan <nurmoin05@gmail.com>
2022-11-21 22:51:13 +0600

    front-end api fetching function createf

---
+++ src/api/productApi.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const getProducts = async () => {
+    const url = `${process.env.REACT_APP_API_BASEURL}/api/v1/get-products`;
+    const res = await axios.get(url);
+    const data = await res.data.data;
+
+    return data;
+}
+
+const addProducts = async (body) => {
+    const url = `${process.env.REACT_APP_API_BASEURL}/api/v1/add-product`;
+    const res = await axios.post(url, body);
+    const data = await res.data;
+    
+    return data
+}
+
+const updateProducts = async (body, id) => {
+    const url = `${process.env.REACT_APP_API_BASEURL}/api/v1/update-product/${id}`;
+    const res = await axios.put(url, body);
+    console.log(res)
+
+}
+
+const deleteProducts = async (id) => {
+    const url = `${process.env.REACT_APP_API_BASEURL}/api/v1/delete-product/${id}`;
+    const res = await axios.delete(url);
+    const data = res.data;
+
+    return data
+}
+
+export {
+    getProducts,
+    addProducts,
+    deleteProducts
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getProducts } from '../../api/productApi';
+
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getProducts();
+            setProducts(data)
+        }
+        fetchData()
+    }, [])
+
+
     return (
         <div>
             This is home page
+            <p>{products.length}</p>
         </div>
     );
 };
