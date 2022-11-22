1b7376275caaa15995114176208098605f31b204
Moin Khan <nurmoin05@gmail.com>
2022-11-22 20:25:34 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Mern Crud</Navbar.Brand>
+                <Nav className="ms-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
