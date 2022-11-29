df3c6bc2848fd5c3d9f888db08c957a68d1ca5cd
Moin Khan <nurmoin05@gmail.com>
2022-11-30 00:26:21 +0600

    form validation added

--- src/pages/AddProducts/AddProducts.js
+++ src/pages/AddProducts/AddProducts.js
@@ -8,7 +8,7 @@ import { addProducts } from "../../api/productApi";
 import uploadImage from "../../api/imageApi";
 
 const AddProducts = () => {
-    const { register, handleSubmit, reset } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
     const navigate = useNavigate();
 
     const onSubmit = async (data) => {
@@ -26,7 +26,7 @@ const AddProducts = () => {
         reset();
         navigate("/");
     }
-
+    console.log(errors)
     return (
         <div className="py-5">
             <PageTitle>Add Product</PageTitle>
@@ -37,8 +37,16 @@ const AddProducts = () => {
                         <Form.Control
                             type="text"
                             placeholder="Enter product name"
-                            {...register("name")}
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: "Name is required!"
+                                }
+                            })}
                         />
+                        <Form.Text className="text-danger fw-semibold fs-6">
+                            {errors?.name?.type === "required" && errors?.name?.message}
+                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="productCode">
@@ -46,16 +54,32 @@ const AddProducts = () => {
                         <Form.Control
                             type="text"
                             placeholder="Enter product code"
-                            {...register("code")}
+                            {...register("code", {
+                                required: {
+                                    value: true,
+                                    message: "Code is required!"
+                                }
+                            })}
                         />
+                        <Form.Text className="text-danger fw-semibold fs-6">
+                            {errors?.code?.type === "required" && errors?.code?.message}
+                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="productImage">
                         <Form.Label>Product Image</Form.Label>
                         <Form.Control
                             type="file"
-                            {...register("image")}
+                            {...register("image", {
+                                required: {
+                                    value: true,
+                                    message: "Image is required!"
+                                }
+                            })}
                         />
+                        <Form.Text className="text-danger fw-semibold fs-6">
+                            {errors?.image?.type === "required" && errors?.image?.message}
+                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="productPrice">
@@ -63,8 +87,21 @@ const AddProducts = () => {
                         <Form.Control
                             type="number"
                             placeholder="Enter product price"
-                            {...register("unitPrice")}
+                            {...register("unitPrice", {
+                                required: {
+                                    value: true,
+                                    message: "Price is required!"
+                                },
+                                min: {
+                                    value: 100,
+                                    message: "Price has to be more than 100!"
+                                }
+                            })}
                         />
+                        <Form.Text className="text-danger fw-semibold fs-6">
+                            {errors?.unitPrice?.type === "required" && errors?.unitPrice?.message}
+                            {errors?.unitPrice?.type === "min" && errors?.unitPrice?.message}
+                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="productQuantity">
@@ -72,8 +109,21 @@ const AddProducts = () => {
                         <Form.Control
                             type="number"
                             placeholder="Enter product Quantity"
-                            {...register("quantity")}
+                            {...register("quantity", {
+                                required: {
+                                    value: true,
+                                    message: "Quantity is required"
+                                },
+                                min: {
+                                    value: 5,
+                                    message: "Quantity has to be more than 5!"
+                                }
+                            })}
                         />
+                        <Form.Text className="text-danger fw-semibold fs-6">
+                            {errors?.quantity?.type === "required" && errors?.quantity?.message}
+                            {errors?.quantity?.type === "min" && errors?.quantity?.message}
+                        </Form.Text>
                     </Form.Group>
 
                     <Button className="w-100 mt-4" variant="primary" type="submit">Submit</Button>
