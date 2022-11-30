162bea95a93912b4d19c6ed38f8aafa6d3b026da
Moin Khan <nurmoin05@gmail.com>
2022-11-30 23:33:52 +0600

    redux installed and configured

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain="nmkhan-dev.us.auth0.com"
-    clientId="h5sVLLkg1MjTgOapnwV2sZy2DMWsR4tp"
-    redirectUri={window.location.origin}
-  >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Auth0Provider>
+  <Provider store={store}>
+    <Auth0Provider
+      domain="nmkhan-dev.us.auth0.com"
+      clientId="h5sVLLkg1MjTgOapnwV2sZy2DMWsR4tp"
+      redirectUri={window.location.origin}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </Provider>
 );
